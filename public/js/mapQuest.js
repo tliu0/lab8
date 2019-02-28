42f8affaf8d31e32a936d602cee4f0407bb6317c
Tiffany Liu <til120@ucsd.edu>
2019-03-01 05:21:41 +0900

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = "24u5oO0fO9UOTRd7GWiGYl5QDVpKIhiy";
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.87880328050148, -117.23591208457947],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//Add a marker to the map
+	L.marker([32.87880328050148, -117.23591208457947]).addTo(map);
 }
